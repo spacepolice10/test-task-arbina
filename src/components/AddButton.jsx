e9a192eff0f0165683d80ef0bf510c28909fe2b8
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-17 16:05:08 +0300

    feat: add addButton element

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,30 @@
+import { Button, Divider, InputGroup } from "@blueprintjs/core";
+import { Popover2, Tooltip2 } from "@blueprintjs/popover2";
+
+function AddInput() {
+    return (
+        <section style={{ display: "flex", padding: 10 }}>
+            <InputGroup />
+            <Divider />
+            <Button icon="arrow-right" />
+        </section>
+    )
+}
+
+export default function AddButton() {
+    return (
+        <Popover2
+            content={<>
+                <AddInput />
+            </>}
+        >
+            <Tooltip2
+                content="Click to add one more string"
+            >
+                <Button
+                    icon="plus"
+                />
+            </Tooltip2>
+        </Popover2>
+    )
+}
