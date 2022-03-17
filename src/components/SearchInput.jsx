94b0ddbb44a66c31948b2c73a2a85ea808301007
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-17 16:05:19 +0300

    feat: add SearchInput element

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,10 @@
+import { InputGroup } from "@blueprintjs/core";
+
+export default function SearchInput() {
+    return (
+        <InputGroup
+            leftIcon="search"
+            placeholder="Search for the input"
+        />
+    )
+}
