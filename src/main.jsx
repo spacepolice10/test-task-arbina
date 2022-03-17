ce7e3f418800038548f1fd02d359c40ea3c7bf62
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-17 14:09:49 +0300

    init: set up project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
