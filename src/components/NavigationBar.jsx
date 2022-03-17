f675dcac6090f803dada2f25300590c2e5c003c0
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-17 16:04:41 +0300

    feat: add navigation panel

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,24 @@
+import { Navbar, NavbarDivider, NavbarGroup, NavbarHeading } from "@blueprintjs/core";
+import AddButton from "./AddButton";
+import SearchInput from "./SearchInput";
+
+
+
+
+export default function NavigationBar() {
+    return (
+        <>
+            <Navbar>
+                <NavbarGroup>
+                    <NavbarHeading>
+                        Test application
+                    </NavbarHeading>
+                    <NavbarDivider />
+                    <SearchInput />
+                    <NavbarDivider />
+                    <AddButton />
+                </NavbarGroup>
+            </Navbar>
+        </>
+    )
+}
