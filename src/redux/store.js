736c152d45b29031c0ea7fabae79442c4ae2f7c1
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-17 16:49:05 +0300

    feat: implement persisten store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createStore, combineReducers } from '@reduxjs/toolkit'
 import tableReducer from './tableSlice/'
 
-export default configureStore({
-    reducer: {
-        table: tableReducer
+const rootReducers = combineReducers({ table: tableReducer });
+
+function saveToLocalStorage(state) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
     }
-})
+}
+function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
+const store = createStore(rootReducers, loadFromLocalStorage())
+store.subscribe(() => saveToLocalStorage(store.getState()));
+export default store;
